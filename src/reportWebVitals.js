01f5dd22050c7f183d62e43ae332b1cc02907289
user1 <user1@DESKTOP-QM3UF42>
2024-02-07 22:17:49 +0200

    123 try

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -4,7 +4,7 @@ const reportWebVitals = onPerfEntry => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
-      //Hello;
+      //Hello;123456789
       getLCP(onPerfEntry);
       getTTFB(onPerfEntry);
     });
